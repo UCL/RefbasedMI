3af895a9c32fcc584cd46c6a2435ad2e68890067
KMcgrathUCL <kevin.mcgrath@ucl.c.uk>
2019-10-01 10:11:15 +0100

    beta parameters into matrix

--- trynorm2.R
+++ trynorm2.R
@@ -1,65 +1,37 @@
-#########################################
-# R program to mimic stata program mimix
-# ie reference based imputation 
-# Note 1st part is to set up a summary table - mimix_group
-# reflects the pattern and treatment group configuration of the raw data
-# then acts as a looping mechanism 
-# norm2 is used as MCMC multivariate normal  
-#########################################
-
-set.seed(101) #so can reproduce results
-# initial program reads stata data files 
 library(haven)
 
 preimputemvn1 <- read_dta("preimputemvn1.dta")
 preimputemvn2 <- read_dta("preimputemvn2.dta")
 
-
-#note refer 2 is to -placebo but in preimputemvn1
-
-#These data sets already prepared in Stata - need to write code to perform here
-# alt read in raw data set 
-asthma <- read_dta("asthma.dta")
-library(tidyr)
-
-#recode treat, Placebo = 1, active =2
- 
-asthma$treatrc<-ifelse(asthma$treat==2,1,2)
-#convert to wide format 
-asthma_wide <-pivot_wider(asthma,id_cols=id,names_from=time,names_prefix="fev",values_from=c(fev))
-#need to add cov col, ie  breaks col
-#asthma_wide$ pivot_wider(asthma,id_cols=id,names_from=time,values_from=c(base),values_fn = list(breaks=mean))
-# can check non missing
-#also need to add the treat col 
-treatrc<-asthma %>% group_by(id) %>%summarise(treatrc=first(treatrc),baseq=first(base)) 
-#bring all the cols together
-asthma_wide<-merge(asthma_wide,treatrc,by= "id")
-# split into treatment data-sets
-#can't have paste on LHS
-for (i in 2:3) {
-paste0("preimpute","i") <-  subset(asthma_wide,treatq==i)
+library("haven")
+preimputemvn <- NULL
+z<-NULL;
+a = c(1,2)
+for(i in a) 
+  {
+  i <- 1:2  
+  preimputemvn <- (paste0("preimputemvn",i,".dta")) 
 }
-
-# 1 is placebo
-preimpute1 <-  subset(asthma_wide,treatrc==1)
-# 2 is active treatment
-preimpute2 <-  subset(asthma_wide,treatrc==2)
-
-
-
+a = c(1,2)
+for(i in a) 
+{
+  preimputemvn[i] <- read_dta(preimputemvn[i])
+}
+  
+Z[1]
 
 
 #Data augmentation da.norm
 #find the starting values by em
 
-#prelim.norm2(preimputemvn1)
-#em.norm()
-library(norm2)
+prelim.norm2(preimputemvn1)
+em.norm()
+
 emNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn1)
 #summary(emNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn1))
 
 emResult1<-emNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn1)
-#set.seed(5321) #so can reproduce results
+set.seed(5321) #so can reproduce results
 
 #mcmcResult <- mcmcNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn1,starting.values = emResult$param)
 
@@ -70,44 +42,6 @@ mcmcResult1<- mcmcNorm(emResult1,iter=5000,multicycle = 100)
 mcmcResult1$param$beta
 #var-cov matrixr 
 mcmcResult1$param$sigma
-
-#try and make the beta parameters matrices , once working change the above
-mat_Betas <-rbind(mcmcResult1$param$beta ,mcmcResult2$param$beta)
-# the 1st parameters are  
-mat_Betas[1,]
-#2nd betas are 
-mat_Betas[2,]
-
-
-#mat_Sigma <-rbind(mcmcResult1$param$sigma ,mcmcResult2$param$sigma)
-
-# store Siigma parametes in matrixlist to enable easy referal
-mat_Sigma <-list(mcmcResult1$param$sigma ,mcmcResult2$param$sigma)
-
-#mat_Sigma[1] is placebo, ie the refernce in this case  
-
-
-########### As a test can take the parameter files from stata
-
-####  1stly just try the means
-
-
-test1 <- c(2.057, 1.953,2.032,1.936,2.13)
-test2 <- c(2.217,2.33,2.312,2.28,2.1)
-mat_Betas <-rbind(test1,test2)
-# and replicate rows! (later)
-
-#same for sigma , use stata values
-
-xlist <-c(0.468,.271,.328,.299,.25,.377,.452,.27,.34,.607,.28,.214,.233,.204,.29)
-
-# this from stata sigma param
-mcmcResult1$param$sigmaTest<-mcmcResult1$param$sigma
-mcmcResult1$param$sigmaTest[,1]<-c(0.468,.271,.328,.299,.25)
-mcmcResult1$param$sigmaTest[,2]<-c(0.271,.377,.452,.27,.34)
-mcmcResult1$param$sigmaTest[,3]<-c(0.328,.452,.607,.28,.214)
-mcmcResult1$param$sigmaTest[,4]<-c(0.299,.27,.28,.233,.204)
-mcmcResult1$param$sigmaTest[,5]<-c(0.25,.34,.214,.204,.29)
 
 i=1
 paste0("mcmcResult",i,"$","param$beta")
@@ -121,8 +55,6 @@ mynames
 
 
 
-
-
 #prelim.norm2(preimputemvn1)
 #em.norm()
 
@@ -130,7 +62,7 @@ emNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn2)
 #summary(emNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn1))
 
 emResult2<-emNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn2)
-
+set.seed(5321) #so can reproduce results
 
 #mcmcResult <- mcmcNorm(cbind(fev1,fev2,fev3,fev4,base) ~1,data=preimputemvn1,starting.values = emResult$param)
 
@@ -176,7 +108,6 @@ case when (is.na(data$fev1) &  !is.na(data$fev2) & !is.na(data$fev3) & !is.na(da
 
 varlist <- c("fev1","fev2","fev3","fev4")
 library(dplyr)
-#found better way then this!
 data$patt <- ifelse(is.na(data$fev1),7,
                       ifelse(is.na(data$fev2),14,
                       ifelse(is.na(data$fev3),12,
@@ -202,8 +133,7 @@ tryagg <- aggregate(sort.datafev,by =list(sort.datafev$treat,sort.datafev$fev1,s
 #convert non nas into 0's?
 #next stage to obtain   
 
-#this works!  to obtain mimix_group
-#but summaris efun migh be nicer as keeps orig var names
+this works!  to obtain mimix_group
 tryagg$patttest <- tryagg$Group.2+tryagg$Group.3*2^1+tryagg$Group.4*2^2+tryagg$Group.5*2^3 
 mimix_group<-data.frame(tryagg[order(tryagg$Group.1),])
 #above works but also need the other variables corresponding to mimix_group
@@ -268,9 +198,6 @@ mcmcResult2$param$sigma
 
 #try to obtain S11 and S12 from sigma matrix using pattern from mimix_group
 
-#hard code
-mimix_group$refer<-1
-
 mimix_group
 #assume covar has no missing values
 mata_miss <- mimix_group[1,c(2,3,4,5)]
@@ -333,21 +260,15 @@ S12 <- mcmcResult1$param$sigma[c_mata_nonmiss,c_mata_miss]
 S22 <- mcmcResult1$param$sigma[c_mata_miss,c_mata_miss]
 
 
-######### START main analysis run HERE ####################################
-
 #CREATE AN EMPTY MATRIX FOR COLLECTING IMPUTED DATA 
 #mata_all_new initialises as a single row with ...'s
-#then recursively added (later)
+#then recursivle added
 mata_all_new <- matrix(nrow=1, ncol=8) #empty matrix
-# if any left over from previous
-# been using this as starter point  
-mata_all_new<-mata_all_new[-c(1:nrow(mata_all_new)),]
 #hard code for now
 names(mata_all_new) <-names(mata_new2)
 
 
-#need to loop thru mimix_group ,define  (nrows(mimix_group) equiv to max_indicator)
-# NOtE as  cyling thru read off the treatment and reference values from relevant cols.   
+#need to loop thru mimix_group ,define  (nrows equiv to max_indicator)
 for ( i in  1:nrow(mimix_group)) 
 {
   mata_miss <- mimix_group[i,c(2,3,4,5)]  #define mata_miss
@@ -364,76 +285,29 @@ for ( i in  1:nrow(mimix_group))
  # use if statement to create suitable mat_obs submat     
   
   c_mata_nonmiss<-which(mata_nonmiss==1)
-
-#j2r requires VAR from reference grp  , 
-# as a list elemt need convert to matrix  
-# here refernce =1  
   
-   SigmaRef <- Reduce(rbind,mat_Sigma[1])
-   
-   S11 <-SigmaRef[c_mata_nonmiss,c_mata_nonmiss]
-   S12 <-SigmaRef[c_mata_nonmiss,c_mata_miss]
-   S22 <-SigmaRef[c_mata_miss,c_mata_miss]
-                  
-   
-#  S11 <- mcmcResult1$param$sigma[c_mata_nonmiss,c_mata_nonmiss]
-#  S12 <- mcmcResult1$param$sigma[c_mata_nonmiss,c_mata_miss]
-#  S22 <- mcmcResult1$param$sigma[c_mata_miss,c_mata_miss]
+  S11 <- mcmcResult1$param$sigma[c_mata_nonmiss,c_mata_nonmiss]
+  S12 <- mcmcResult1$param$sigma[c_mata_nonmiss,c_mata_miss]
+  S22 <- mcmcResult1$param$sigma[c_mata_miss,c_mata_miss]
   
   print("S12 i = " )
   print(S12)
   print(i)
   
-# need expand mata_means to same no rows as counter but only after deriving mata_Means
-# for  J2r I believe is case of taking relevant treatment betas and overwriting when missing by reference treatment if different     
+ # need expand meta_means to same no rows as counter
   
-# count of pattern by treatment
-  cnt<- mimix_group$X1[i]
-# treatment grp
-  trt_gp<- mimix_group$Grouptrt[i]
-# reference grp
-  refer <- mimix_group$refer[i] 
-# try to mcmcrEsult as 2 matrices  
-   
-   #DFbeta<-mcmcResult[refer]1$param$beta
-   DFbeta<-mat_Betas[trt_gp,]
-   
-# want the treatment grp means and where missing overwrite with refer grp (when differs!)     
-   mata_means_trt <-mat_Betas[trt_gp,] 
-   mata_means_ref <-mat_Betas[refer,]
- 
-#one way is to element multiply (because 1,0) then add 
-   mata_means_t <- mata_means_trt*mata_nonmiss
-   mata_means_r <- mata_means_ref*mata_miss
-# so when all missing  1,1,1, ... then all contributing comes from reference means      
-   mata_means <- mata_means_r+mata_means_t
-# and preserve names   
-   names(mata_means) <- names(mata_means_trt)
-
-# replicate to number of rows defined by X1 
-   mata_means<-mata_means[rep(seq(nrow(mata_means)),each=mimix_group$X1[i]),]
-   
-   
-# these are the replications   
- #  betasX1<- DFbeta[rep(seq(nrow(DFbeta)),each=mimix_group$X1[i]),] 
- #   print("betasX1")
- #   print(betasX1)
+   mimix_group$X1[i]
+    DFbeta<-mcmcResult1$param$beta
+   betasX1<- DFbeta[rep(seq(nrow(DFbeta)),each=mimix_group$X1[i]),] 
+   print("betasX1")
+   print(betasX1)
     #matrix(rep(mcmcResult1$param$beta,each=10,nrow=10))
 # i not incrementing below so comment out !   
 #}
 
 #now can go to *MNAR IMPUTATION equlv section:  but within the i loop
-#J2R
-# uses means treatment group and for missing, replace with refer group means   
-   
 
-   
-
-
-   
-   # mat_Betas
-   #m1<- betasX1[c_mata_nonmiss]
-   #m2<- betasX1[c_mata_miss]
+#m1 is means dimensioned by nonmiss and m2 by miss matrices
 
 #raw1 is found from mata_obs  fom the sorted data (ie mimix_d2) by subsetting on the pattern 
 #also reorder index col from 1st to  last
@@ -444,11 +318,10 @@ mimix_d2cols <- mimix_d2c[,c(1:4,7,ncol(mimix_d2c))]
 
 
 #then mata_obs is the sequential selection of rows according to the mimix_group variable X1 values
-# stil goimg thru the rows in mimix_group want to read off the X1 values to create subsets   
-#for (l in 1:mimix_group[i,"X1"]) {
-    j <- mimix_group[i,"X1"]
-    print("j=")
-    print(j)
+for (l in 1:mimix_group[i,"X1"]) {
+   # j <- mimix_group[i,"X1"]
+    print("l=")
+    print(l)
    # mata_obs <- mimix_d2cols[i:j,]
   
 #need a counter to accumulate j  - easiest way is to ceate a cumulstive col in mimix_group
@@ -468,9 +341,6 @@ mimix_d2cols <- mimix_d2c[,c(1:4,7,ncol(mimix_d2c))]
    print("mata_obs = ")
    print(mata_obs)
 #} 
-   
-   #J2r uses Sigma derived from refernce group
-   #Mean 
  
 #NOTE in stata no t calcs as no missing pattern for 1st 37 obs. so need insert If statement somewhere!
  if(length(c_mata_miss)!=0 ) {
@@ -494,19 +364,10 @@ mimix_d2cols <- mimix_d2c[,c(1:4,7,ncol(mimix_d2c))]
     #qnorm takes inverse cdf
   #seed enusre get same result each time
     set.seed(101)
-    
-    m1 <- mata_means[c_mata_nonmiss]
-    m2 <- mata_means[c_mata_miss]
-  print("mata_means = ")
-  print(mata_means) 
-      
     Z<-qnorm(matrix(runif( mimix_group[i,"X1"]* mimix_group[i,"sum_1s"],0,1),mimix_group[i,"X1"],mimix_group[i,"sum_1s"]))
-  meanval = as.matrix(m2) + as.matrix(raw1 - m1)%*%as.matrix(t_mimix)
+  meanval = m2 + as.matrix(raw1 - m1)%*%as.matrix(t_mimix)
   mata_y1 = meanval+Z%*%t(U)
-
-  # betas correctly programmed as used stata values in test, difference is in t, ie cholsolve, ie S11 S12 matrices
   
-    
   print(mata_y1)         
   # define mata_new
   # mata_new 1st consists of mat_obs (same as is used mata_nonmiss, seems unnecessary calculation)
@@ -538,6 +399,6 @@ mimix_d2cols <- mimix_d2c[,c(1:4,7,ncol(mimix_d2c))]
   mata_all_new <-bind_rows(mata_all_new,mata_new2)
   print(mata_all_new)
  }
-
+}
    
    
