533ec82c8f159f4f384837d613e64bfac1f2fa1b
KMcgrathUCL <45361270+KMcgrathUCL@users.noreply.github.com>
2019-12-05 16:44:22 +0000

    add main program  and functions  ..R

---
+++ functions.R
@@ -0,0 +1,170 @@
+
+
+
+library(tidyr)
+#pivot_wider
+library(dplyr)
+#for select
+library(plyr)
+#read data
+# need haven
+library(haven)
+library(pastecs)
+library(norm2)
+library(sparseinv) 
+# trying to write a subroutine for this part of data preprocessing
+A <- function(x) {  ifelse(!is.na(x),0,1) }
+
+
+# this section to convert from long to wide data format
+preprodata<- function(pathdata,depvar,treatvar,idvar,timevar,covar,M,refer,meth)  {
+  #extract relevant vars
+  #mxdata <-read.csv("./asthma.csv")
+  csvdata <-read.csv(pathdata)
+  fevdata<-select(csvdata,idvar,depvar,timevar)
+  # only want to widen the dose var, ie fev, so take the other variables
+  #reshape to wide and assign new names 
+  # prefix must be supplied from input argument rather than hard coded, this canbe hard coded
+  sts4<-pivot_wider(fevdata,id_cols=c(idvar),names_from=timevar,names_prefix=depvar,values_from=depvar)
+  #sts4 is just response data, can join later treat and covar cols from finaldat 
+  #assumes no NA for these vars should add in checking routines !!
+  uniqdat<-unique(mxdata[c(idvar,covar,treatvar)])
+  
+  
+  finaldat<- merge(sts4,uniqdat,by=idvar)
+  # now try and sort on treatvar, doesnt work so instead of sorting , select on treat
+ 
+  
+  # need find no. ntreat  to loop over
+  #ntreat<-unique(unique(mxdata$treatvar))
+  ntreatcol<-(select(mxdata,treatvar))
+  ntreat <- unique(ntreatcol)
+  ntimecol<-(select(mxdata,timevar))
+  ntime<-unique(ntimecol)
+  
+  
+  #in order to aggregate by pattern need create dummy vars
+  
+  # for sts4 array drop 1st col and create newvars for rest
+  STSdummy<-apply(sts4[,2:ncol(sts4)],MARGIN=2,FUN=A)
+  # append to names
+  colnames(STSdummy) <- paste(colnames(STSdummy),'.1')
+  # merge back on to data 
+  sts4D<-cbind(finaldat,STSdummy)
+  # create powrs of 2
+  pows2 <- sapply(1:ncol(STSdummy),function(i) STSdummy[,i]*2^(i-1))
+  #need to add up to find patt
+  patt <- rowSums(pows2)
+  #sts4Dpatt<-cbind(sts4D,patt)
+  sts4Dpatt<-cbind(sts4D,patt)
+  
+  # then sort by patt( last col) and treat and find cumulative sequence  AND split on treatment var!
+  # so need to merge treat (and covar) back into response data 
+  
+  #sort by treatvar and patt var, couldnt get treatvar working with patt so try sorting previously on treatvar   
+  finaldatSS <-sts4Dpatt[order(patt),] 
+  
+  ndx = order(finaldatSS[,treatvar])
+  finaldatS <- finaldatSS[ndx,]
+  
+  # consistent with Stata to get right order, drop id and treat cols
+  drops <-c(idvar,treatvar)
+  
+  
+  #also get the dummy pattern vectors by treatment
+  #dummypatt<-unique(xSTSdummy)
+  pattmat<-unique(STSdummy[,1:ncol(STSdummy)])
+  
+  # sort of hard coded for now may have to revisit this!
+  pows2d <- sapply(1:ncol(pattmat),function(i) pattmat[,i]*2^(i-1)) 
+  patt<-rowSums(pows2d)
+  #want  join this to ex from mimix_group table
+  #merge(uniqdat,sts4,by=idvar)
+  pattmatd<-cbind(pattmat,patt)
+  
+  # this best for  count, implicit sort by treat
+  # may have to generalise using function argument  later!
+  ex1 <- sts4Dpatt %>%
+    dplyr::group_by(sts4Dpatt$treat,sts4Dpatt$patt) %>%
+    dplyr::summarise(X1 =n())
+  ex1 <- dplyr::rename(ex1,treat='sts4Dpatt$treat',patt='sts4Dpatt$patt')
+  # and now find cumX1
+  ex1$X1cum <- cumsum(ex1$X1) 
+  #want  join this to ex from mimix_group table
+  # create index number to preserve ordr after merge
+  ex1$exid <- 1:nrow(ex1)
+  ex1id <-merge(ex1,pattmatd,by="patt")
+  ex1s<-ex1id[order(ex1id$exid),]
+  
+ 
+  for (val in t(ntreat)) {
+    print(paste0("prenormMC",val))
+    #assign(paste0("prenorm2",val),subset(finaldat,treat==val))
+  } 
+  
+  return(list(sts4Dpatt,finaldatS,finaldat,ntreat,ex1s,ex1,ex1id,pattmat,patt,ntime,M,refer,meth))
+}
+# Question is should the wide data be sorted for analysis or just ordered in fact, selecting out treatmet and rbinding? to provide analysis data set.   
+# answer is should be sorted by patt as in Stata!
+# therefore just select on treat when looping thru ntreat 
+
+
+mi_impute <-function(idvar,timevar,depvar,covar) {
+  #preprodata(depvar,treatvar,idvar,timevar,covar)
+  tst<-pivot_wider(mxdata,id_cols=c(idvar),names_from=timevar,names_prefix=depvar,values_from=depvar)
+  # add ont covariates and add on to resp list
+  respvars<-c(names(tst[,-1]),covar)
+  return(respvars)
+}
+
+
+# analse function to be used after main run to output  for summary stats
+analse <- function(meth,no)  {
+  assign( paste0("mata_all_new_rmna",meth), na.omit(mata_all_new))
+  assign( paste0("mata_all_new_rmna",meth,no) , filter(get(paste0("mata_all_new_rmna",meth)),SNO == no))
+  t(round(stat.desc(get(paste0("mata_all_new_rmna",meth,no))[,c("fev2","fev4","fev8","fev12")]),3)[c(1,9,13,4,8,5),])
+}
+
+# mata_S_miss something like [2 3 4] ,so is cc
+CIR_loop <- function(c_mata_miss,mata_Means,MeansC)
+{    
+  miss_count <- length(c_mata_miss)
+  mata_means <- mata_Means
+  
+  for (b in 1:miss_count)  {
+    
+    # if 1st col missing then no value before so need to check for that
+    # note mimix counter just no rows in each patt
+    # so main looping is over missin fields, ie miss_count
+    # if 1st col must be ref  
+    if (c_mata_miss[b] ==1) {   
+      #print will cause objectto return 
+      #print(paste0(miss_count))
+      #for each patt_count (from mimix_group) 
+      #test purposes
+      #count<-2
+      
+      mata_means[b] <- MeansC[b] 
+    } else  {
+      mata_means[c_mata_miss[b]] = mata_means[(c_mata_miss[b]-1)]+ MeansC[(c_mata_miss[b])]- MeansC[(c_mata_miss[b])-1]
+    } 
+  }
+  return(mata_means)
+}
+
+pttestf<- function(n1,n2,mn1,sd1,mn2,sd2) {
+  pttest = pt((((mn1 - mn2) -0) /sqrt(sd1^2/n1+sd2^2/n2)),(n1+n2-2))
+  return(pttest)
+}
+
+LMCF_loop <- function(c_mata_miss,mata_Means)
+{
+  miss_count <- length(c_mata_miss)
+  mata_means <- mata_Means
+  for (b in 1:miss_count)  {
+    if (c_mata_miss[b] > 1) {
+      mata_means[c_mata_miss[b]] <- mata_means[(c_mata_miss[b]-1)]      
+    } 
+  }
+  return(mata_means)
+}
---
+++ main.R
@@ -0,0 +1,370 @@
+# need also input refer
+refer <-2
+
+testlist<-preprodata("asthma.csv","fev","treat","id","time","base",1000,2,"MAR")
+
+
+# returns list from preprodata function
+ntreat<-testlist[[4]]
+finaldatS<-testlist[[2]]
+mg<-testlist[[5]]
+# vital to get the mata_obs correctly sorted! so corresponds with mimix_group lookup 
+# to be consistent with Stata move the base col after the fevs!
+mata_Obs <- testlist[[2]]
+M <- testlist[[11]]
+refer <- testlist[[12]]
+meth <- testlist[[13]]
+
+# so tsts2 is th equiv to mi_impute so try to put in norm2
+tst2 <- mi_impute("id","time","fev","base")
+# put commas in
+tst3<-paste(tst2,collapse = ",")
+
+for (val in t(ntreat)) {
+  print(paste0("prenormdat",val))
+  assign(paste0("prenormdat",val),subset(finaldatS,treat==val))
+}  
+
+
+
+#*CREATE AN EMPTY MATRIX FOR COLLECTING IMPUTED DATA 	
+# just an empty row but take dimensions and col types fron mata_obs
+#mata_all_new<-array(data=1,dim=c(1,2+ncol(mata_Obs)))
+GI<-c(0)
+II<-c(0)
+SNO <-mata_Obs[1,1]
+dropid<-c("id")
+mata_ObsX<-mata_Obs[,!(names(mata_Obs) %in% dropid)] 
+mata_all_new <- cbind(GI,II,mata_ObsX,SNO)
+mata_all_new[ mata_all_new>=0] <-NA 
+
+
+
+
+# so tsts2 is th equiv to mi_impute so try to put in norm2
+tst2 <- mi_impute("id","time","fev","base")
+# put commas in
+tst3<-paste(tst2,collapse = ",")
+
+for (val in t(ntreat)) {
+  print(paste0("prenormdat",val))
+  assign(paste0("prenormdat",val),subset(finaldatS,treat==val))
+}  
+
+# run the mcmc simulations over the treatment grps 
+for (val in t(ntreat)) {
+  #prnormobj <- subset(prenormdat2, select=c(tst2))
+  #prnormobj <- assign(paste0("prenormdat",val),subset(finaldatS,treat==val))
+  kmvar=get(paste0("prenormdat",val))
+  prnormobj<-assign(paste0("prnormobj",val), subset(kmvar, select=c(tst2)))
+  for(m in 1:M) {  
+    #emResultT<-emNorm(testprnormobj,prior = "ridge",prior.df=0.5)
+    emResultT<-emNorm(prnormobj,prior = "jeffreys")
+    mcmcResultT<- mcmcNorm(emResultT,iter=1000,multicycle = NULL,prior = "jeffreys")
+    #assign(paste0("mcmcResultT",val,m),mcmcResultT)
+    assign(paste0("parambeta",val,m),mcmcResultT$param$beta)
+    assign(paste0("paramsigma",val,m),mcmcResultT$param$sigma)
+    print(paste0("mcmcNorm finished"))
+    #print(paste0("parambeta",val,m))
+    #return(list(paste0("parambeta",val,m),paste0("paramsigma",val,m)))
+  }                                 
+}
+
+
+# now loop over the lookup table mg, looping over every pattern
+for (i in 1:nrow(mg))
+{
+  #define mata_miss as vector of 1's denoting missing using col names ending i ".1"
+  mata_miss <- mg[i,grep("*..1",colnames(mg)),drop=F]
+  #mata_miss <- mimix_group[i,c(2,3,4,5)]     #define mata_miss
+  #assumes covariate non missing
+  mata_miss$covar.1 <-0                       #assuming cov col is non missing
+  mata_nonmiss <- (ifelse(mata_miss==0,1,0))  #define mata-nonmiss from miss
+  
+  # need transform nonmiss,miss to c lists - ie. index the  
+  c_mata_miss<-which(mata_miss==1)
+  c_mata_nonmiss<-which(mata_nonmiss==1)
+  # eg no missing is c(1,2,3,4,5)
+  
+  
+  # count of pattern by treatment
+  cnt<- mg$X1[i]
+  
+  # treatment grp
+  trtgp<- mg$treat[i]
+  cat("trtgp = ", trtgp)
+  
+# multiple  simulations start here within the pattern loop #########  
+for ( m in  1:M)  { 
+    #*FOR INDIVIDUALS WITH NO MISSING DATA COPY COMPLETE DATA INTO THE NEW DATA MATRIX mata_all_new `m' TIMES
+    #if `pat' == 0{ 
+    if(length(c_mata_miss)==0 ) {
+      st<-mg[i,"X1cum"]-mg[i,"X1"]+1
+      en <-mg[i,"X1cum"]
+      SNO<-mata_Obs[c(st:en),1]
+      mata_new <- mata_Obs[c(st:en),2:ncol(mata_Obs)]
+      GI <- array(data=mg[i,"treat"],dim=c(mg[i,"X1"],1))
+      #II  no imputations 
+      II <- array(data=m,dim=c(mg[i,"X1"],1))
+      mata_new=cbind(GI,II,mata_new,SNO)
+      #names(mata_all_new)<-names(mata_new)
+      mata_all_new=rbind(mata_all_new,mata_new)
+    } else {
+   
+    #FOR INDIVIDUALS WITH  MISSING DATA  `m' TIMES  
+    # dependent on method chosen 
+
+        if (meth== 'MAR')  {
+  
+               mata_means <- get(paste0("parambeta",trtgp,m))  
+  # mata_means<-mata_means[rep(seq(nrow(mata_means)),each=mg$X1[i]),]
+  
+  
+               Sigmatrt <- get(paste0("paramsigma",trtgp,m))
+               S11 <-Sigmatrt[c_mata_nonmiss,c_mata_nonmiss]
+               S12 <-Sigmatrt[c_mata_nonmiss,c_mata_miss]
+               S22 <-Sigmatrt[c_mata_miss,c_mata_miss]
+  
+  
+        }
+     else if (meth == 'J2R') { 
+               mata_means_trt <- get(paste0("parambeta",trtgp,m)) 
+               mata_means_ref <- get(paste0("parambeta",refer,m))
+       
+               mata_means <- get(paste0("parambeta",trtgp,m))
+               #one way is to element multiply (because 1,0) then add 
+               mata_means_t <- unlist(mata_means_trt)*mata_nonmiss
+               mata_means_r <- unlist(mata_means_ref)*mata_miss
+               # so when all missing  1,1,1, ... then all contributing comes from reference means      
+               mata_means <- mata_means_r+mata_means_t
+               # and preserve names   
+               colnames(mata_means) <- colnames(mata_means_trt)
+               #replicate to number of rows defined by X1 
+              #mata_means<-mata_means[rep(seq(nrow(mata_means)),each=mg$X1[i]),]
+  
+  
+  ############# SIGMA is from paramsigma  the reference group ################
+  
+  
+           # do we ever  use SigmaTrt !!?? in j2r? 
+            SigmaRefer <- get(paste0("paramsigma",refer,m))
+  # when reading in Stata sigmas
+  # needs to to ths as tibble will fail in cholesky
+  #SigmaRefer <- as.matrix(get(paste0("paramsigmaStata",refer,m)))
+  
+            print(paste0("paramsigma",refer,m))
+  #SigmaRefer <- Reduce(rbind,res1sigma.list[m])
+  
+            S11 <-SigmaRefer[c_mata_nonmiss,c_mata_nonmiss]
+            S12 <-SigmaRefer[c_mata_nonmiss,c_mata_miss]
+            S22 <-SigmaRefer[c_mata_miss,c_mata_miss]
+  
+          }
+              else if (meth=='CR') {
+              mata_means <- get(paste0("parambeta",refer,m))
+  #mata_means_r <- unlist(mata_means_ref)*1
+  #mata_means_r <- unlist(mata_means_ref)*mata_miss
+  #mata_means <- mata_means_r
+  # and preserve names   
+  #colnames(mata_means) <- colnames(mata_means_trt)
+  #replicate to number of rows defined by X1 
+  #mata_means <- mata_means_r
+  #colnames(mata_means) <- colnames(mata_means_ref)
+  
+  #mata_means<-mata_means[rep(seq(nrow(mata_means)),each=mg$X1[i]),]
+  
+                SigmaRefer <- get(paste0("paramsigma",refer,m))
+                S11 <-SigmaRefer[c_mata_nonmiss,c_mata_nonmiss]
+                S12 <-SigmaRefer[c_mata_nonmiss,c_mata_miss]
+                S22 <-SigmaRefer[c_mata_miss,c_mata_miss]
+         }
+           else if (meth=='CIR') {
+  # pre-deviating use mean of trt gp up to last obs time bfore deviating, post-deviating use mean from ref grp 
+  
+  #mata_means_trt <- get(paste0("parambeta",trtgp,m)) 
+  #mata_means_ref <- get(paste0("parambeta",refer,m))
+  
+            # put equiv to mimix 
+              mata_Means <-  mata_means_trt
+              MeansC <- mata_means_ref
+  
+  #might be better to copy mimix algol
+  
+             mata_means<-CIR_loop(c_mata_miss,mata_Means,MeansC)
+  #returns mata_means as single row
+  # then duplicate over patt rows
+  #replicate to number of rows defined by X1 
+  # mata_means<-mata_means[rep(seq(nrow(mata_means)),each=mg$X1[i]),]
+  
+             SigmaRefer <- get(paste0("paramsigma",refer,m))
+  # when reading in Stata sigmas
+  # needs to to ths as tibble will fail in cholesky
+  #SigmaRefer <- as.matrix(get(paste0("paramsigmaStata",refer,m)))
+  
+             print(paste0("paramsigma",refer,m))
+  #SigmaRefer <- Reduce(rbind,res1sigma.list[m])
+  
+             S11 <-SigmaRefer[c_mata_nonmiss,c_mata_nonmiss]
+             S12 <-SigmaRefer[c_mata_nonmiss,c_mata_miss]
+             S22 <-SigmaRefer[c_mata_miss,c_mata_miss]
+            }  
+              else if (meth=='LMCF') { 
+                
+              mata_Means <-  get(paste0("parambeta",trtgp,m))
+              # no ref MeansC <- mata_means_ref
+               mata_means<-LMCF_loop(c_mata_miss,mata_Means)
+              #mata_means<-mata_means[rep(seq(nrow(mata_means)),each=mg$X1[i]),]
+  
+             Sigmatrt <- get(paste0("paramsigma",trtgp,m))
+                 # when reading in Stata sigmas
+              S11 <-Sigmatrt[c_mata_nonmiss,c_mata_nonmiss]
+              S12 <-Sigmatrt[c_mata_nonmiss,c_mata_miss]
+              S22 <-Sigmatrt[c_mata_miss,c_mata_miss]
+     }  #if meth
+      
+   
+
+# loop still open for row(mg)
+
+###################### MNAR IMPUTATION ################
+# need insert routine when ALL missing values
+# else
+###################### MNAR IMPUTATION ################
+
+
+#make sure these are single row vectors! as mistake in LMCF but have to be duplicate rows so add ,s
+#and move after dup fun
+      #Error in mata_means[, c_mata_nonmiss] : incorrect number of dimensions
+#m1 <- mata_means[c_mata_nonmiss]
+#m2 <- mata_means[c_mata_miss]
+   mata_means<-mata_means[rep(seq(nrow(mata_means)),each=mg$X1[i]),]
+   if (!is.null(nrow(mata_means)) )  {
+   m1 <- mata_means[,c_mata_nonmiss]
+   m2 <- mata_means[,c_mata_miss]
+   } else {
+     m1 <- mata_means[c_mata_nonmiss]
+     m2 <- mata_means[c_mata_miss]
+   }
+
+
+#then mata_obs is the sequential selection of rows according to the mimix_group variable X1 values
+
+#need a counter to accumulate j  - easiest way is to ceate a cumulstive col in mimix_group
+
+
+
+     j <- mg[i,"X1"]
+     print(paste0(" count in patt = ", j))
+     k <- mg[i,"X1cum"]
+     startrow <-(k-j+1)  
+      stoprow  <-(k)
+      print("startrow stoprow = ")
+      print(startrow)
+      print(stoprow)
+
+#raw1 <- mata_obs[, c_mata_nonmiss]
+     preraw <- mata_Obs[c(startrow:stoprow),2:ncol(mata_Obs)]
+     raw1 <- preraw[,c_mata_nonmiss]
+     print("mata_raw = ")
+
+
+    t_mimix =cholsolve(Q=S11,y=S12)   
+    conds <-  S22-t(S12)%*%t_mimix
+#  
+     #meanval = as.matrix(m2) + as.matrix(raw1 - m1)%*%as.matrix(t_mimix)     
+     # below for CR but checks work with J2R
+     meanval = (m2) + as.matrix(raw1 - m1)%*%as.matrix(t_mimix)
+     U <- chol(conds)
+      # mg[i,X1] is equiv to Stata counter, miss_count is no. of missing, so 
+    miss_count=rowSums(mata_miss)
+    # gen erate inverse normal
+     Z<-qnorm(matrix(runif( mg[i,"X1"]* miss_count,0,1),mg[i,"X1"],miss_count))
+     # check same input parameters for inverse norm gen as in stata 
+     print(paste0('mg[i,X1] =',mg[i,"X1"],' miss_count= ',miss_count)) 
+
+     mata_y1 = meanval+Z%*%t(U)
+
+     #define new matrix from observed,  id column  (the last)
+    mata_new <- preraw
+
+      # assigning the columns where the missing values  
+      if(length(c_mata_miss)==0 ) { mata_new <- mata_Obs[,c(2:nct+1)]
+      }else {
+      mata_new[,c_mata_miss] <- mata_y1
+      }
+# if(length(c_mata_miss)!=0 )
+
+#save U,Z for imp(m) and patt(i)
+# this worked for Z
+
+    #what i this below? 
+    #presumablywas  for testing?
+#assign(paste0("Z11",i,"_imp",m),subset(Z))
+#assign(paste0("U11",i,"_imp",m),subset(U))
+        # assign(paste0("S12",i,"_imp",m),S12)
+        # assign(paste0("S22",i,"_imp",m),S22)
+        # assign(paste0("mata_y1_11",i,"_imp",m),mata_y1)
+        # assign(paste0("conds11",i,"_imp",m),conds) 
+# no idea why below cuases error!
+        # assign(paste0("t_mimix11",i,"_imp",m),t_mimix)
+
+#assuming this  from Stata if "`interim'"==""{  
+         # SNO just id col
+         SNO <- mata_Obs[c(startrow:stoprow),1]
+          #SNO <- mata_ObsX[,ncol(mata_Obs)]
+          # GI treatment grp column 1 (here),II imputation number col, mata_new matrix then SNO is id col.
+         GI <- array(data=mg[i,1],dim=c(mg[i,"X1"],1))
+          #II  no imputations 
+          II <- array(data=m,dim=c(mg[i,"X1"],1))
+
+#doesnt need SNO, as id already in
+#mata_new<-cbind(GI,II,mata_new,SNO)
+          mata_new<-cbind(GI,II,mata_new,SNO)
+          mata_all_new<-rbind(mata_all_new,mata_new)
+#stata equilv \ is row bind NOt cbind!!?
+    
+    } #if patt ==0r
+  } #for row[mg] 
+ 
+} #for M StOP HERE!!
+
+analse(meth,"5456")
+pttestf(1000,1000,1.621,0.289,1.657,0.388)
+
+
+analse <- function(meth,no)  {
+ assign( paste0("mata_all_new_rmna",meth), na.omit(mata_all_new))
+ assign( paste0("mata_all_new_rmna",meth,no) , filter(get(paste0("mata_all_new_rmna",meth)),SNO == no))
+ t(round(stat.desc(get(paste0("mata_all_new_rmna",meth,no))[,c("fev2","fev4","fev8","fev12")]),3)[c(1,9,13,4,8,5),])
+}
+
+
+    
+
+mata_all_new_rmnaJ2R <- na.omit(mata_all_new)
+mata_all_new_rmnaJ2R5456 <- filter(mata_all_new_rmnaJ2R,SNO == "5456")
+t(round(stat.desc(mata_all_new_rmnaJ2R5456[,c("fev2","fev4","fev8","fev12")]),3)[c(1,9,13,4,8,5),])
+
+
+analysemeth <- function(meth,no) {
+     return(assign(paste0("mata_all_new_rmnaT",meth), (na.omit(mata_all_new) )) )
+    # return(datana)
+}
+ 
+analyseSNO <- function()    
+     #datanaSNO<-assign(paste0(datana,no), filter(datana,SNO == no))
+     #datana<-assign(paste0(data,"na",meth), na.omit(data))
+     return(datana)
+    }
+analyse(meth,5456)     
+     
+     
+     datanaSNO<-assign(paste0("mata_all_new_rmnaJ2R","5456"), filter(mata_all_new_rmnaJ2R,SNO == 5456))
+     descstats<- t(round(stat.desc(datanaSNO[,c("fev2","fev4","fev8","fev12")]),3)[c(1,9,13,4,8,5),])
+     return(descstats)
+     }
+analyse(mata_all_new,meth,5456)  
+kmtest<-assign(paste0("mata_all_new_rmna",meth), na.omit(mata_all_new))
+
+
+
