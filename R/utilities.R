07b363601c0491b6d88f0c9c11acdbf0de3381d3
KMcgrathUCL <45361270+KMcgrathUCL@users.noreply.github.com>
2020-01-07 17:24:03 +0000

    Add files via upload

---
+++ R/utilities.R
@@ -0,0 +1,17 @@
+
+# for select function
+install.packages("dplyr")
+library(dplyr)
+
+# for pivot_wider function
+install.packages("tidyr")
+library(tidyr)
+
+# for emNorm
+
+install.packages("norm2")
+library(norm2)
+
+# for cholsolve
+install.packages("sparseinv")
+library("sparseinv")
