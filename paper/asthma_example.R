c931d5d14c969a4dfe410fc4a26ea3b2282deb4e
ianrwhite <35139755+ianrwhite@users.noreply.github.com>
2021-03-29 15:39:00 +0100

    Delete folder 'paper'

--- paper/asthma_example.R
+++
@@ -1,5 +0,0 @@
-asthmaJ2R <- mimix(data=asthma, covar='base', depvar=fev, treatvar=treat, idvar=id, timevar=time,
-                   method='J2R', reference=1, M=5, seed=101, prior=ridge, burnin=1000)
-library(mice)     
-fit <- with(data= as.mids(asthmaJ2R), lm(fev.12 ~ treat + base))
-summary(pool(fit))
