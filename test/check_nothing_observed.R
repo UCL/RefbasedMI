8ef5da0f80b5d8ea720c3b6014e7f39aa234e388
ianrwhite <35139755+ianrwhite@users.noreply.github.com>
2022-01-04 14:00:14 +0000

    Create check_nothing_observed.R

---
+++ test/check_nothing_observed.R
@@ -0,0 +1,94 @@
+# check_nothing_observed.R
+# IW 4/1/2022
+#
+# explore how individuals with no outcomes are handled
+# do this by adding a large number to all in treat=2, 
+#   making one individual in treat=2 wholly missing, 
+#   and viewing their imputed values
+
+
+# select the individual
+idc = min(asthma[asthma$treat==2,"id"])
+
+
+# modify the data
+asthma2 <- asthma
+asthma2[asthma2$treat==2,"fev"] <- asthma2[asthma2$treat==2,"fev"] + 10000
+asthma2[asthma2$treat==2 & asthma2$id==idc,"fev"] <- NA
+
+asthma  %>% filter(id==idc)
+asthma2 %>% filter(id==idc)
+
+# impute various ways
+impnooutcomes2j2r <- RefBasedMI(data=asthma2,
+                                depvar=fev,
+                                treatvar=treat,
+                                idvar=id,
+                                timevar=time,
+                                M=2,
+                                method="j2r",
+                                reference=1,
+                                seed=101,
+                                prior="jeffreys",
+                                burnin=1000,
+                                bbetween=NULL,
+                                methodvar=NULL,
+                                delta=c(1,2,3,4), dlag=c(.1,.1,.1,.05)
+)
+
+impnooutcomes2cir <- RefBasedMI(data=asthma2,
+                                depvar=fev,
+                                treatvar=treat,
+                                idvar=id,
+                                timevar=time,
+                                M=2,
+                                method="cir",
+                                reference=1,
+                                seed=101,
+                                prior="jeffreys",
+                                burnin=1000,
+                                bbetween=NULL,
+                                methodvar=NULL,
+                                delta=c(1,2,3,4), dlag=c(.1,.1,.1,.05)
+)
+
+impnooutcomes2lmcf <- RefBasedMI(data=asthma2,
+                             depvar=fev,
+                             treatvar=treat,
+                             idvar=id,
+                             timevar=time,
+                             M=2,
+                             method="lmcf",
+                             reference=1,
+                             seed=101,
+                             prior="jeffreys",
+                             burnin=1000,
+                             bbetween=NULL,
+                             methodvar=NULL,
+                             delta=c(1,2,3,4), dlag=c(.1,.1,.1,.05)
+)
+
+impnooutcomes2causal <- RefBasedMI(data=asthma2,
+                             depvar=fev,
+                             treatvar=treat,
+                             idvar=id,
+                             timevar=time,
+                             M=2,
+                             method="causal", K0=0.8, K1=1,
+                             reference=1,
+                             seed=101,
+                             prior="jeffreys",
+                             burnin=1000,
+                             bbetween=NULL,
+                             methodvar=NULL,
+                             delta=c(1,2,3,4), dlag=c(.1,.1,.1,.05)
+)
+
+# view imputed values
+impnooutcomes %>% filter(id==idc) 
+impnooutcomes2j2r %>% filter(id==idc) 
+impnooutcomes2cir %>% filter(id==idc) 
+impnooutcomes2lmcf %>% filter(id==idc) 
+impnooutcomes2causal %>% filter(id==idc) 
+
+# note only lmcf imputes 10000 larger, consistent with only lmcf using the own-arm mean
